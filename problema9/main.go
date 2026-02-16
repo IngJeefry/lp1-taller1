f753f031f7914c9e0e09af560db3efc23c628e11
IngJeefry <remingtonpruebas@gmail.com>
2026-02-16 00:54:05 -0500

    solución1 al problema9_Orden Global (Tomar el de menor ID primero

--- problema9/main.go
+++ problema9/main.go
@@ -10,26 +10,57 @@ import (
 // Hay 5 filósofos y 5 tenedores (recursos). Cada filósofo necesita 2 tenedores para comer.
 // Estrategia segura: imponer un **orden global** al tomar los tenedores (primero el menor ID, luego el mayor)
 // para evitar deadlock. También puedes limitar concurrencia (ej. mayordomo).
-// TODO: completa la lógica de toma/soltado de tenedores y bucle de pensar/comer.
+// completa la lógica de toma/soltado de tenedores y bucle de pensar/comer.
 
 type tenedor struct{ mu sync.Mutex }
 
 func filosofo(id int, izq, der *tenedor, wg *sync.WaitGroup) {
-	// TODO: desarrolla el código para el filósofo
-	
+	// desarrolla el código para el filósofo
+	defer wg.Done()
+
+	for i := 0; i < 3; i++ {
+		pensar(id)
+
+		if id < (id+1) %5 {
+			// Orden: primero el de menor ID
+            fmt.Printf("[filósofo %d] esperando tenedor %d...\n", id, id)
+            izq.mu.Lock()
+            fmt.Printf("[filósofo %d] tomó tenedor %d\n", id, id)
+            
+            fmt.Printf("[filósofo %d] esperando tenedor %d...\n", id, (id+1)%5)
+            der.mu.Lock()
+            fmt.Printf("[filósofo %d] tomó tenedor %d\n", id, (id+1)%5)
+        } else {
+            // Orden inverso: primero el de menor ID (que sería el derecho)
+            fmt.Printf("[filósofo %d] esperando tenedor %d...\n", id, (id+1)%5)
+            der.mu.Lock()
+            fmt.Printf("[filósofo %d] tomó tenedor %d\n", id, (id+1)%5)
+            
+            fmt.Printf("[filósofo %d] esperando tenedor %d...\n", id, id)
+            izq.mu.Lock()
+            fmt.Printf("[filósofo %d] tomó tenedor %d\n", id, id)
+        }
+        
+        comer(id)
+        
+        // Liberar tenedores (orden inverso al que los tomamos)
+        der.mu.Unlock()
+        izq.mu.Unlock()
+        fmt.Printf("[filósofo %d] soltó tenedores\n", id)
+	}
 	fmt.Printf("[filósofo %d] satisfecho\n", id)
 }
 
 func pensar(id int) {
 	fmt.Printf("[filósofo %d] pensando...\n", id)
-	// TODO: simular tiempo de pensar
-
+	// simular tiempo de pensar
+	time.Sleep(time.Duration(500+id*100) * time.Millisecond)
 }
 
 func comer(id int) {
 	fmt.Printf("[filósofo %d] COMIENDO\n", id)
-	// TODO: simular tiempo de pensar
-
+	// simular tiempo de pensar
+	time.Sleep(time.Duration(300+id*50) * time.Millisecond)
 }
 
 func main() {
@@ -40,16 +71,16 @@ func main() {
 	// crear tenedores
 	forks := make([]*tenedor, n)
 	for i := 0; i < n; i++ {
-		// TODO: inicializar cada tenedor i
-
+		// inicializar cada tenedor i
+		forks[i] = &tenedor{}
 	}
 
 	// lanzar filósofos
 	for i := 0; i < n; i++ {
 		izq := forks[i]
 		der := forks[(i+1)%n]
-		// TODO: lanzar goroutine para el filósofo i
-
+		// lanzar goroutine para el filósofo i
+		go filosofo(i, izq, der, &wg)
 	}
 
 	wg.Wait()
